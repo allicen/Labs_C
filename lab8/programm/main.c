d41b8463e24fb022ba07ebd013064ad7da3f1ff2
allicen <ellena.hella@yandex.ru>
2022-11-18 11:53:47 +0300

    Задание 8

---
+++ lab8/programm/main.c
@@ -0,0 +1,17 @@
+/*
+-D __USE_MINGW_ANSI_STDIO 
+-fexec-charset=CP866
+-O2
+-Wall
+-Wextra
+-pedantic
+-s
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	return 0;
+}
