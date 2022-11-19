8e99f2801739c65b2a74f53110102786bb6af2b5
allicen <ellena.hella@yandex.ru>
2022-11-19 15:42:09 +0300

    Задание 12

--- lab12/programm/main.c
+++ lab12/programm/main.c
@@ -13,42 +13,41 @@
 #include <stdlib.h>
 
 int minValue(int a, int b) {
-	if (a <= b) {
-		return a;
-	}
-	
-	return b;
+    if (a <= b) {
+        return a;
+    }
+    
+    return b;
 }
 
 char compareValue(int a, int b) {
-	if (a < b) {
-		return '<';
-	} else if (a == b) {
-		return '=';
-	}
-	
-	return '>';
+    if (a < b) {
+        return '<';
+    } else if (a == b) {
+        return '=';
+    }
+    
+    return '>';
 }
 
 int numberPow(int num, int pow) {
-	if (pow > 0)  {
-		return num * numberPow(num, pow - 1);
-	}
-	return 1;
+    if (pow > 0)  {
+        return num * numberPow(num, pow - 1);
+    }
+    return 1;
 }
 
 
 int main(void) {
-	
-	// 1 задание 
-	printf("Минимальное значение из minValue: %d\n", minValue(50, 5));
-	
-	// 2 задание
-	printf("Результат сравнения в compareValue: %c\n", compareValue(2, 5));
-	
-	// 3 задание
-	printf("Результат возведения в степень в numberPow: %d\n", numberPow(2, 4));
-	
-	
-	return 0;
+    
+    // 1 задание 
+    printf("Минимальное значение из minValue: %d\n", minValue(50, 5));
+    
+    // 2 задание
+    printf("Результат сравнения в compareValue: %c\n", compareValue(2, 5));
+    
+    // 3 задание
+    printf("Результат возведения в степень в numberPow: %d\n", numberPow(2, 4));
+    
+    return 0;
 }
