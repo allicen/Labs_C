8c43e54053e4052f170a1dd494133c12a3abcd6f
allicen <ellena.hella@yandex.ru>
2022-11-19 20:32:54 +0300

    Задание 13

--- lab13/programm/main.c
+++ lab13/programm/main.c
@@ -52,6 +52,54 @@ int* sort(int *arr, int size, int max) {
 }
 
 
+int* minMax(int *arr, int size) {
+	int *resMinMax = malloc(2 * sizeof(int));
+	
+	if (!resMinMax) {
+		return NULL;
+	}
+	
+	for (int i = 0; i < size; i++) {
+		if (i == 0) {
+			resMinMax[0] = arr[i];
+			resMinMax[1] = arr[i];
+			continue;
+		}
+		
+		if (arr[i] < resMinMax[0]) {
+			resMinMax[0] = arr[i];
+		} else if (arr[i] > resMinMax[1]) {
+			resMinMax[1] = arr[i];
+		}
+	}
+	
+	return resMinMax;
+} 
+
+
+
+int rowMaxValueNymber(int table[][4], int rowCount, int colCount) {
+	
+	int rowWithMax = 0, maxNum = 0;
+	for (int i = 0; i < rowCount; i++) {
+		for (int j = 0; j < colCount; j++) {
+
+			if (i == 0 && j == 0) {
+				maxNum = table[i][j];
+				continue;
+			}
+			
+			if (table[i][j] > maxNum) {
+				maxNum = table[i][j];
+				rowWithMax = i;
+			}
+		}
+	}
+	
+	return rowWithMax;
+}
+
+
 int main(void) {
 	int arr[] = {2, 5, 7, 6, 0, 6};
 	int n = sizeof(arr)/sizeof(arr[0]);
@@ -69,13 +117,25 @@ int main(void) {
     	for (int i = 0; i < n; i++) {
 			printf("%d ", arrSort[i]);
 		}
+		printf("\n");
         free(arrSort);
     }
 	
 	
 	// 3 задание
+	int *arrMinMax = minMax(arr, n);
+	if (arrMinMax) {
+		printf("Минимальное число в массиве arr: %d\n", arrMinMax[0]);
+		printf("Максимальное число в массиве arr: %d\n", arrMinMax[1]);
+		free(arrMinMax);
+	}
 	
 	// 4 задание
+	int table[][4] = {{1, 2, 5, 600}, {5, 8, 9, 1000}, {9000, 8, 1, 7}};
+	int tableRowCount = sizeof(table)/sizeof(table[0]);
+	int rowNumberWithMaxNumber = rowMaxValueNymber(table, tableRowCount, 4);
+	printf("Максимальное число содержится в строке №%d\n", rowNumberWithMaxNumber);
+	
 	
 	// 5 задание
 	
